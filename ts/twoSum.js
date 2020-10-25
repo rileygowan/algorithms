b073bea32fec30cbdb3343168665ca2db4034052
Riley Gowan <t.rileygowan@gmail.com>
2020-10-25 13:48:46 -0700

    Formatting

--- ts/twoSum.js
+++ ts/twoSum.js
@@ -1,15 +1,14 @@
-"use strict";
+'use strict';
 var twoSum = function (nums, target) {
-    var indices = new Map();
-    return nums.reduce(function (result, number, index) {
-        var remainder = target - number;
-        var storedIndex = indices.get(remainder);
-        if (storedIndex >= 0) {
-            return [storedIndex, index];
-        }
-        else {
-            indices.set(number, index);
-        }
-        return result;
-    }, []);
+  var indices = new Map();
+  return nums.reduce(function (result, number, index) {
+    var remainder = target - number;
+    var storedIndex = indices.get(remainder);
+    if (storedIndex >= 0) {
+      return [storedIndex, index];
+    } else {
+      indices.set(number, index);
+    }
+    return result;
+  }, []);
 };
