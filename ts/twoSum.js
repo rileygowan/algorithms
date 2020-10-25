2b3019e8c920ef888925a431daf23c7319def4af
Riley Gowan <rgowan@shutterstock.com>
2020-10-25 13:47:32 -0700

    tsconfig

---
+++ ts/twoSum.js
@@ -0,0 +1,15 @@
+"use strict";
+var twoSum = function (nums, target) {
+    var indices = new Map();
+    return nums.reduce(function (result, number, index) {
+        var remainder = target - number;
+        var storedIndex = indices.get(remainder);
+        if (storedIndex >= 0) {
+            return [storedIndex, index];
+        }
+        else {
+            indices.set(number, index);
+        }
+        return result;
+    }, []);
+};
