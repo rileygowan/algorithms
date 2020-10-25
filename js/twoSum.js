74bad7c2611cb61d856ed044f2a82364f25640be
Riley Gowan <rgowan@shutterstock.com>
2020-10-25 10:45:29 -0700

    Functional response

--- js/twoSum.js
+++ js/twoSum.js
@@ -14,3 +14,13 @@ const twoSum = (nums, target) => {
   }
   return [];
 };
+
+// const twoSum = (nums, target) => nums.reduce(twoSumReducer(target), []);
+// const twoSumReducer = (target) => (result, current, index, nums) => {
+//   if (result.length) return result;
+//   const remainder = target - current;
+//   if (nums.indexOf(remainder) > -1 && nums.indexOf(remainder) !== index) {
+//     return [nums.indexOf(remainder), index];
+//   }
+//   return result;
+// };
